90f581a8d89e4aaf3641a89d62487ce1e61bfc1c
densee <48784519+densee@users.noreply.github.com>
2021-04-10 03:12:41 +0900

    upload week-4

--- code/week-4/README.md
+++ code/week-4/README.md
@@ -8,23 +8,104 @@
 
 ## Assignment
 
-You will complete the implementation of a simple particle filter by writing the following two methods of `class ParticleFilter` defined in `particle_filter.py`:
+1. I put the observed data calculated by map frame into global_observation and the data observed  by vehicle into local_observation.
+2. When I calculated multivariate normal pdf, I put observation data as input state, distance between landmark and particle as mean. 
+3. After that, we calculated the weight of the particles and updated them through resampling.
+The contents below are the contents of the code file I wrote.
+    
 
-* `update_weights()`: For each particle in the sample set, calculate the probability of the set of observations based on a multi-variate Gaussian distribution.
-* `resample()`: Reconstruct the set of particles that capture the posterior belief distribution by drawing samples according to the weights.
 
-To run the program (which generates a 2D plot), execute the following command:
+    def update_weights(self, sensor_range, std_landmark_x, std_landmark_y,observations, map_landmarks):    
+        for p in self.particles:
+            visible_landmarks = []
+            global_observations = []
+            local_observations = []
 
-```
-$ python run.py
-```
+            for map_landmark in map_landmarks:
+                if distance(p, map_landmarks[map_landmark]) <= sensor_range:
+                    map_landmark_x = map_landmarks[map_landmark]['x']
+                    map_landmark_y = map_landmarks[map_landmark]['y']
+                    map_landmark_id = map_landmark
 
-Without any modification to the code, you will see a resulting plot like the one below:
+                    visible_landmark = {
+                        'id': map_landmark_id,
+                        'x': map_landmark_x,
+                        'y': map_landmark_y,
+                    }
+                    visible_landmarks.append(visible_landmark)
 
-![Particle Filter without Proper Update & Resample][empty-update]
+            for observation in observations:
+                global_observation_x = p['x'] + observation['x']*np.cos(p['t']) - observation['y']*np.sin(p['t'])
+                global_observation_y = p['y'] + observation['x']*np.sin(p['t']) + observation['y']*np.cos(p['t'])
+                local_observation_x = observation['x']
+                local_observation_y = observation['y']
 
-while a reasonable implementation of the above mentioned methods (assignments) will give you something like
+                global_observation = {
+                    'x': global_observation_x,
+                    'y': global_observation_y,
+                }
 
-![Particle Filter Example][example]
+                local_observation = {
+                    'x': local_observation_x,
+                    'y': local_observation_y,
+                }
 
-Carefully read comments in the two method bodies and write Python code that does the job.
+                local_observations.append(local_observation)
+                global_observations.append(global_observation)
+
+            if len(visible_landmarks) == 0:
+                continue
+
+            p['assoc'] = []
+            p['w'] = 1.0
+
+            association_landmarks = self.associate(visible_landmarks, global_observations)
+
+            for i in range(len(association_landmarks)):
+                cov = [[std_landmark_x**2, 0],
+                       [0, std_landmark_y**2]]
+                arr_cov = np.array(cov)
+
+                k = 2
+
+                tmp_obs = [local_observations[i]['x'],
+                           local_observations[i]['y']]
+                arr_obs = np.array(tmp_obs)
+
+                tmp_assoc = [association_landmarks[i]['x']-p['x'],
+                             association_landmarks[i]['y']-p['y']]
+                arr_assoc = np.array(tmp_assoc)
+
+                p['w'] *= self.multivariate_normal_pdf(arr_obs, arr_assoc, cov, k)
+                p['assoc'].append(association_landmarks[i]['id'])
+
+    # Resample particles with replacement with probability proportional to
+    #   their weights. 
+
+
+    def multivariate_normal_pdf(self, x, mean, cov, k):
+        x_m = x - mean
+        cov_det = abs(np.linalg.det(cov))
+        one_over_sqrt_2pi = 1 / (np.sqrt(((2 * np.pi) ** k) * cov_det))
+        return one_over_sqrt_2pi * np.exp(-0.5 * np.dot(np.dot(np.transpose(x_m), np.linalg.inv(cov)), x_m))
+
+    def resample(self):
+        copied_particle = []
+        weights = [p['w'] for p in self.particles]
+
+        N = len(weights)
+        positions = (np.arange(len(weights)) + np.random.random()) / N
+
+        indexes = np.zeros(N, 'i')
+        cumulative_sum = np.cumsum(weights)
+        i, j = 0, 0
+
+        while i < N and j < N:
+            if positions[i] < cumulative_sum[j]:
+                indexes[i] = j
+                i += 1
+            else:
+                j += 1
+
+        copied_particle.append(self.particles[indexes[i]])
+        self.particles = copied_particle
--- code/week-4/particle_filter.py
+++ code/week-4/particle_filter.py
@@ -1,4 +1,6 @@
 import numpy as np
+from math import exp
+from numpy.random import uniform
 from helpers import distance
 
 class ParticleFilter:
@@ -26,6 +28,7 @@ class ParticleFilter:
     # Add measurements to each particle and add random Gaussian noise.
     def predict(self, dt, velocity, yawrate, std_x, std_y, std_theta):
         # Be careful not to divide by zero.
+        cnt = 0
         v_yr = velocity / yawrate if yawrate else 0
         yr_dt = yawrate * dt
         for p in self.particles:
@@ -70,35 +73,99 @@ class ParticleFilter:
         #   the list of (coordinates transformed) predictions.
         return associations
 
+    def norm_pdf(self, x, m, s):
+        one_over_sqrt_2pi = 1 / np.sqrt(2 * np.pi)
+        return (one_over_sqrt_2pi / s) * exp(-0.5 * ((x - m) / s) ** 2)
+
+    def bivariate_pdf(self, x1, x2, y1, y2, s1, s2):
+        #Bivariate Normal Distribution
+        cor = 0.0
+        one_over_sqrt_2pi = 1 / np.sqrt(2 * np.pi*s1*s2)
+        tmp = ((x1-x2)**2/s1**2) + ((y1-y2)**2/s2**2) -2*cor*((x1-x2)*(y1-y2)/s1*s2)
+        return one_over_sqrt_2pi * exp(-0.5 * tmp)
+
+    def calculate_dist(self, value1, value2):
+        dist = (value1**2 + value2**2)**0.5
+        return dist
+
+    def multivariate_normal_pdf(self, x, mean, cov, k):
+        x_m = x - mean
+        cov_det = abs(np.linalg.det(cov))
+        one_over_sqrt_2pi = 1 / (np.sqrt(((2 * np.pi) ** k) * cov_det))
+        return one_over_sqrt_2pi * np.exp(-0.5 * np.dot(np.dot(np.transpose(x_m), np.linalg.inv(cov)), x_m))
+
     # Update the weights of each particle using a multi-variate
     #   Gaussian distribution.
-    def update_weights(self, sensor_range, std_landmark_x, std_landmark_y,
-                       observations, map_landmarks):
-        # TODO: For each particle, do the following:
-        # 1. Select the set of landmarks that are visible
-        #    (within the sensor range).
-        # 2. Transform each observed landmark's coordinates from the
-        #    particle's coordinate system to the map's coordinates.
-        # 3. Associate each transformed observation to one of the
-        #    predicted (selected in Step 1) landmark positions.
-        #    Use self.associate() for this purpose - it receives
-        #    the predicted landmarks and observations; and returns
-        #    the list of landmarks by implementing the nearest-neighbour
-        #    association algorithm.
-        # 4. Calculate probability of this set of observations based on
-        #    a multi-variate Gaussian distribution (two variables being
-        #    the x and y positions with means from associated positions
-        #    and variances from std_landmark_x and std_landmark_y).
-        #    The resulting probability is the product of probabilities
-        #    for all the observations.
-        # 5. Update the particle's weight by the calculated probability.
+    def update_weights(self, sensor_range, std_landmark_x, std_landmark_y, observations, map_landmarks):
 
-        pass
+
+        for p in self.particles:
+            visible_landmarks = []
+            global_observations = []
+            local_observations = []
+
+            for map_landmark in map_landmarks:
+                if distance(p, map_landmarks[map_landmark]) <= sensor_range:
+                    map_landmark_x = map_landmarks[map_landmark]['x']
+                    map_landmark_y = map_landmarks[map_landmark]['y']
+                    map_landmark_id = map_landmark
+
+                    visible_landmark = {
+                        'id': map_landmark_id,
+                        'x': map_landmark_x,
+                        'y': map_landmark_y,
+                    }
+                    visible_landmarks.append(visible_landmark)
+
+            for observation in observations:
+                global_observation_x = p['x'] + observation['x']*np.cos(p['t']) - observation['y']*np.sin(p['t'])
+                global_observation_y = p['y'] + observation['x']*np.sin(p['t']) + observation['y']*np.cos(p['t'])
+                local_observation_x = observation['x']
+                local_observation_y = observation['y']
+
+                global_observation = {
+                    'x': global_observation_x,
+                    'y': global_observation_y,
+                }
+
+                local_observation = {
+                    'x': local_observation_x,
+                    'y': local_observation_y,
+                }
+
+                local_observations.append(local_observation)
+                global_observations.append(global_observation)
+
+            if len(visible_landmarks) == 0:
+                continue
+
+            p['assoc'] = []
+            p['w'] = 1.0
+
+            association_landmarks = self.associate(visible_landmarks, global_observations)
+
+            for i in range(len(association_landmarks)):
+                cov = [[std_landmark_x**2, 0],
+                       [0, std_landmark_y**2]]
+                arr_cov = np.array(cov)
+
+                k = 2
+
+                tmp_obs = [local_observations[i]['x'],
+                           local_observations[i]['y']]
+                arr_obs = np.array(tmp_obs)
+
+                tmp_assoc = [association_landmarks[i]['x']-p['x'],
+                             association_landmarks[i]['y']-p['y']]
+                arr_assoc = np.array(tmp_assoc)
+
+                p['w'] *= self.multivariate_normal_pdf(arr_obs, arr_assoc, cov, k)
+                p['assoc'].append(association_landmarks[i]['id'])
 
     # Resample particles with replacement with probability proportional to
     #   their weights.
+
     def resample(self):
-        return
         # TODO: Select (possibly with duplicates) the set of particles
         #       that captures the posteior belief distribution, by
         # 1. Drawing particle samples according to their weights.
@@ -108,7 +175,26 @@ class ParticleFilter:
         # Finally, self.particles shall contain the newly drawn set of
         #   particles.
 
-        pass
+        copied_particle = []
+        weights = [p['w'] for p in self.particles]
+
+        N = len(weights)
+        positions = (np.arange(len(weights)) + np.random.random()) / N
+
+        indexes = np.zeros(N, 'i')
+        cumulative_sum = np.cumsum(weights)
+        i, j = 0, 0
+
+        while i < N and j < N:
+            if positions[i] < cumulative_sum[j]:
+                indexes[i] = j
+                i += 1
+            else:
+                j += 1
+
+        copied_particle.append(self.particles[indexes[i]])
+        self.particles = copied_particle
+
 
     # Choose the particle with the highest weight (probability)
     def get_best_particle(self):
